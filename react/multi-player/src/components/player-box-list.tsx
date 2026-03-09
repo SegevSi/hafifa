dfaff45e9f4190db16d2d0663b880d968a06a7fd
StudentSKT202 <segev8si@gmail.com>
2026-03-09 17:16:05 +0200

    fix only 2 =

--- react/multi-player/src/components/player-box-list.tsx
+++ react/multi-player/src/components/player-box-list.tsx
@@ -10,7 +10,7 @@ export default function PlayerBoxList({ selectedPlayerId, players, selectPlayer 
             <PlayerBox 
                 key={player.id}
                 player={player}
-                isSelected={player.id == selectedPlayerId}  
+                isSelected={player.id === selectedPlayerId}  
                 selectPlayer={() => selectPlayer(player.id)}  
             />
         ))}
