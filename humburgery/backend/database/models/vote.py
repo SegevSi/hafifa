1fa974538d51769392d6fe266570ce3ec833120f
StudentSKT202 <segev8si@gmail.com>
2026-03-14 02:56:08 +0200

    fix delete dish on cascade

--- humburgery/backend/database/models/vote.py
+++ humburgery/backend/database/models/vote.py
@@ -10,8 +10,7 @@ class Vote(Document):
     class Settings:
         name = "votes"
         keep_nulls = False
-        use_state_management = True
-        cascade = ["dish", "user"]
+
 
 
 # from custom_types import PyObjectId
--- humburgery/backend/repositories/dish_repository.py
+++ humburgery/backend/repositories/dish_repository.py
@@ -10,7 +10,7 @@ async def create_dish(dish: Dish) -> Dish:
 
 
 async def delete_dish(dish_id: PydanticObjectId) -> None:
-    dish = await Dish.get(dish_id)
+    dish = await Dish.find_one({"_id": dish_id}, fetch_links=True)
 
     if not dish:
         raise NotFoundException(f"Could not delete dish, dish with id {dish_id} was not found")
