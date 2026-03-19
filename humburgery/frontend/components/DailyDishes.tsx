a2f8bdf0e527fa2b53cd5db86ce552f6ad40159e
StudentSKT202 <segev8si@gmail.com>
2026-03-19 17:24:13 +0200

    add scroll

--- humburgery/frontend/components/DailyDishes.tsx
+++ humburgery/frontend/components/DailyDishes.tsx
@@ -2,6 +2,7 @@ import { DailyDishesProp } from "@/types";
 import styles from "../styles/DailyDishes.module.css";
 import { proccesDate } from "../utils/date";
 import DishCard from "./DishCard";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 
 export default function DailyDishes({ title, dishes, date }: DailyDishesProp) {
@@ -13,14 +14,14 @@ export default function DailyDishes({ title, dishes, date }: DailyDishesProp) {
                 <div className={styles.title}>{title}</div>
                 <div className={styles.date}>{proccesDate(date)}</div>
             </div>
-            <div className={styles.dishesRow}>
+            <ScrollContainer className={styles.dishesRow}>
                 {dishes.map(dish => 
                     <DishCard
                         key={dish.id}
                         {...dish}
                     />
                 )}
-            </div>
+            </ScrollContainer>
         </div>
     );
 }
