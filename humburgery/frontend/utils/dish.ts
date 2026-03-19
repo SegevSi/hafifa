b0d295d749b2e2b20e7c777c667611c05af8923a
StudentSKT202 <segev8si@gmail.com>
2026-03-19 13:16:33 +0200

    front: add util dish funcs procces

--- humburgery/frontend/utils/dish.ts
+++ humburgery/frontend/utils/dish.ts
@@ -1,4 +1,4 @@
-import type { Dish, DailyDishesProp } from "../types";
+import type { Dish, DailyDishesProp, UpdateDish, DishRequest } from "../types";
 import { DaysData } from "@/conf";
 
 
@@ -6,9 +6,11 @@ const addDish = (oldDishes: Dish[], newDish: Dish): Dish[] => {
     return [...oldDishes, newDish];
 };
 
+
 const deleteDish = (oldDishes: Dish[], dish_id: string): Dish[] => {
     return oldDishes.filter(dish => dish.id !== dish_id);
 };
+
 
 const updateDish = (oldDishes: Dish[], updatedDish: Dish): Dish[] => {
     return oldDishes.map(dish => {
@@ -20,14 +22,62 @@ const updateDish = (oldDishes: Dish[], updatedDish: Dish): Dish[] => {
 };
 
 
-const proccesDishes = (dishes: D)
+const getGroupedDishesByDayOfWeek = (dishes: Dish[]): Dish[][] => {
+    const dishesByDayOfWeek: Dish[][] = new Array(DaysData.DAYS.length).fill([]);
+
+    dishes.forEach(dish => {
+        dishesByDayOfWeek[dish.day_of_week].push(dish);
+    });
+
+    return dishesByDayOfWeek;
+};
 
 
+const proccesDishes = (dishes: Dish[]): DailyDishesProp[] => {
+    const dishesByDayOfWeek = getGroupedDishesByDayOfWeek(dishes);
+
+    const currDate = new Date();
+    const dailyDishesArr = [];
+    const todayDishes = {
+        title: `${DaysData.DAYS[currDate.getDay()]} ,${DaysData.TODAY}`,
+        date: new Date(currDate),
+        dishes: dishesByDayOfWeek[currDate.getDay()]
+    };
+    dailyDishesArr.push(todayDishes);
+
+    for (let index = 0; index < DaysData.DAYS.length - 1; index++) {
+        currDate.setDate(currDate.getDate() + 1);
+
+        dailyDishesArr.push({
+            title: DaysData.DAYS[currDate.getDay()],
+            date: new Date(currDate),
+            dishes: dishesByDayOfWeek[currDate.getDay()]
+        });
+    }
+
+    return dailyDishesArr;
+};
+
+
+const proccesDishForUpdate = (oldDish: Dish, newDish: DishRequest): UpdateDish => {
+    const updateDish: UpdateDish = {};
+
+    let key: keyof DishRequest;
+
+    for (key in newDish) {
+        if (oldDish[key] !== newDish[key])
+            Object.assign(updateDish, {key: newDish[key]});
+    } 
+
+    return updateDish;
+};
 
 export {
     addDish,
     deleteDish,
     updateDish,
+    proccesDishes,
+    proccesDishForUpdate,
 };
 
 
